b0dc998f1868e978981f9165be51b13afd1171b1
Gonzalo Ramos <gnzramos@gmail.com>
2022-05-29 01:19:06 -0500

    Dando forma al sitio

---
+++ 04--Javascript-desde-cero/js/header.js
@@ -0,0 +1,15 @@
+fetch("./partials/nav.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("header").innerHTML = data;
+  });
+
+/* fetch("./footer.html")
+  .then(response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("footer").innerHTML = data;
+  }); */
